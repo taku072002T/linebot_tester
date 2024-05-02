fa7de2123476ef6dc5ac46c9e8a52e86aa10c96e
taku072002 <taku07200@gmail.com>
2024-05-02 19:55:38 +0900

    Created projects

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { Client, middleware } from "@line/bot-sdk";
+
+const config = {
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+};
+const client = new Client(config);
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.post("/", middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent)).then((result) =>
+    res.json(result)
+  );
+});
+
+app.listen(PORT);
+
+function handleEvent(event) {
+  return client.replyMessage(event.replyToken, {
+    type: "text",
+    text: event.message.text,
+  });
+}
+  if (event.type !== "message" || event.message.type !== "text") {
+    return Promise.resolve(null);
+  }
+
